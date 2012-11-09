b4340643cd296f1e4eba4b1b6a881eab2d71ef02
Sam Deane <sam@elegantchaos.com>
2012-11-09 19:16:41 +0000

    updated documentation

--- KSMockServer.h
+++ KSMockServer.h
@@ -9,20 +9,21 @@
 #define MockServerAssert(x) assert((x))
 
 /**
-    A server which runs locally and "pretends" to be something else.
 
-    You provide the server with an optional port to run on, and a list of responses.
+ A server which runs locally and "pretends" to be something else.
 
-    The responses consist of an array of arrays, in this format:
-    pattern, command, command...
+ You provide the server with an optional port to run on, and a list of responses.
 
-    The pattern is a regular expression which is matched against input received by the server.
-    The commands are NSString, NSData, or NSNumber objects, which are processed when
-    the pattern has been matched.
+ The responses consist of an array of arrays, in this format:
+ pattern, command, command...
 
-    See the [documentation](http://karelia.github.com/MockServer/Documentation/).
+ The pattern is a regular expression which is matched against input received by the server.
+ The commands are NSString, NSData, or NSNumber objects, which are processed when
+ the pattern has been matched.
 
-    See also the MockServerTests.m file for some examples.
+ See the [documentation](http://karelia.github.com/MockServer/Documentation/).
+
+ See also the MockServerTests.m file for some examples.
 
  */
 
@@ -59,8 +60,7 @@
 /**
  Make a server that uses the given responses, and listens on a given port.
 
- @note The system doesn't always free up ports instantly, so if you run multiple 
-            tests on a fixed port in quick succession you may find that the server fails to bind.
+ @note The system doesn't always free up ports instantly, so if you run multiple tests on a fixed port in quick succession you may find that the server fails to bind.
 
  @param port The port to listen on.
  @param responses An array of responses that the server uses to respond to connections.
@@ -72,8 +72,7 @@
 /**
  Initialise with a given set of responses, listening on a given port.
 
- @note The system doesn't always free up ports instantly, so if you run multiple
- tests on a fixed port in quick succession you may find that the server fails to bind.
+ @note The system doesn't always free up ports instantly, so if you run multiple tests on a fixed port in quick succession you may find that the server fails to bind.
 
  @param port The port to listen on.
  @param responses An array of responses that the server uses to respond to connections.
@@ -98,10 +97,10 @@
  Loop in the current run loop until something calls stop on the server.
  
  Typically you start the server, initiate the network operation that you want
- to test, then call runUntilStopped in your test case. 
+ to test, then call [KSMockServer runUntilStopped] in your test case. 
 
  In a completion block or delegate method of your network operation, you 
- can then call stop on the server, at which point your test case will continue
+ can then call [KSMockServer stop] on the server, at which point your test case will continue
  executing and you can verify that you got the results you were expecting.
  
  */
@@ -119,5 +118,17 @@
 
 @end
 
+/**
+ When the server encounters this as an output item,
+ it closes the connection.
+ */
+
 extern NSString *const CloseCommand;
+
+/**
+ The server interprets a response with this key specially.
+ When the connection is first opened, the data associated
+ with this key is immediately sent back.
+ */
+
 extern NSString *const InitialResponseKey;
