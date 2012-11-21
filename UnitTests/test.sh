399d9f028d4cb551c615386e51794dcc0ab14f11
Sam Deane <sam@elegantchaos.com>
2012-11-21 22:15:41 +0000

    added OCUnit2JUnit, improved test script

--- UnitTests/test.sh
+++ UnitTests/test.sh
@@ -4,12 +4,18 @@ base=`dirname $0`
 echo "$base"
 pushd "$base/.." > /dev/null
 build="$PWD/test-build"
+ocunit2junit="$base/OCUnit2JUnit/bin/ocunit2junit"
 popd > /dev/null
 
 sym="$build/sym"
 obj="$build/obj"
 
+testout="$build/output.log"
+testerr="$build/error.log"
+
 rm -rf "$build"
 mkdir -p "$build"
 
-xcodebuild -workspace "KSMockServer.xcworkspace" -scheme "Unit Tests" -sdk "macosx" -config "Debug" test OBJROOT="$obj" SYMROOT="$sym" # > "$testout" 2> "$testerr"
+xcodebuild -workspace "KSMockServer.xcworkspace" -scheme "Unit Tests" -sdk "macosx" -config "Debug" test OBJROOT="$obj" SYMROOT="$sym" > "$testout" 2> "$testerr"
+cd "$build"
+"../$ocunit2junit" < "$testout"
