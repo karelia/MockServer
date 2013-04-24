a1424f4e8b1c38d1aa17ad43ff178632727877ca
Sam Deane <sam@elegantchaos.com>
2013-04-24 11:57:18 +0100

    Updated test script for iOS target and changed scheme names.

--- UnitTests/test.sh
+++ UnitTests/test.sh
@@ -3,7 +3,6 @@
 # This script builds and runs the unit tests and produces output in a format that is compatible with Jenkins.
 
 base=`dirname $0`
-echo "$base"
 pushd "$base/.." > /dev/null
 build="$PWD/test-build"
 ocunit2junit="$base/OCUnit2JUnit/bin/ocunit2junit"
@@ -18,6 +17,37 @@ testerr="$build/error.log"
 rm -rf "$build"
 mkdir -p "$build"
 
-xcodebuild -workspace "MockServer.xcworkspace" -scheme "Unit Tests" -sdk "macosx" -config "Debug" test OBJROOT="$obj" SYMROOT="$sym" > "$testout" 2> "$testerr"
-cd "$build"
-"../$ocunit2junit" < "$testout"
+testMac()
+{
+	echo "Testing Mac"
+	xcodebuild -workspace "MockServer.xcworkspace" -scheme "Unit Tests OS X" -sdk "macosx" -config "Debug" test OBJROOT="$obj" SYMROOT="$sym" > "$testout" 2> "$testerr"
+	if [ $? != 0 ]; then
+		echo "Mac build failed"
+		cat "$testerr"
+	else
+		cd "$build"
+		"../$ocunit2junit" < "$testout"
+		cd ..
+	fi
+}
+
+testIOS()
+{
+	# have to build the project/target, not the workspace/scheme, since xcodebuild doesn't support
+	# running unit tests on the ios simulator via workspace/scheme, for some bizarre reason
+	echo "Testing iOS"
+	xcodebuild -project "MockServer.xcodeproj" -target "UnitTests iOS" -sdk "iphonesimulator" -arch i386 -config "Debug" build OBJROOT="$obj" SYMROOT="$sym" TEST_AFTER_BUILD=YES > "$testout" 2> "$testerr"
+	if [ $? != 0 ]; then
+		echo "iOS build failed"
+		cat "$testerr"
+	else
+		cd "$build"
+		"../$ocunit2junit" < "$testout"
+		cd ..
+	fi
+}
+
+
+testIOS
+testMac
+
