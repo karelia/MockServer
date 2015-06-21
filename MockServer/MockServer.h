19d8a5c0c9e4651a53394b22e06275f4f14b0128
Terrence Talbot <ttalbot@karelia.com>
2015-06-20 20:54:41 -0700

    add MockServer framework target

---
+++ MockServer/MockServer.h
@@ -0,0 +1,19 @@
+//
+//  MockServer.h
+//  MockServer
+//
+//  Created by Terrence Talbot on 6/20/15.
+//  Copyright (c) 2015 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MockServer.
+FOUNDATION_EXPORT double MockServerVersionNumber;
+
+//! Project version string for MockServer.
+FOUNDATION_EXPORT const unsigned char MockServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MockServer/PublicHeader.h>
+
+
